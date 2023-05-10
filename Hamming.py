3350232556038b9060623e8ec895cd1dd46ecb64
BryanTYTan <90520318+BryanTYTan@users.noreply.github.com>
2023-05-09 21:38:06 -0500

    Add files via upload

---
+++ Hamming.py
@@ -0,0 +1,38 @@
+import math
+
+def StringToBinary(stringy):
+    tempt, listo = [], []
+    # convert char to ascii number
+    for i in stringy:
+        tempt.append(ord(i))
+
+    # convert ascii numbers to binary
+    for i in tempt:
+        listo.append(int(bin(i)[2:]))
+
+    return listo
+
+def BinaryToString(listo):
+    stringy = ""
+
+    return stringy
+
+# prep data for transfer
+def hamming(listo):
+
+    return listo
+
+# interpret transfered data
+def Dehamming(listo):
+
+    return listo
+
+def main():
+    stringy = "hi"
+    data = StringToBinary(stringy)
+    print(data)
+    return
+
+
+# call program
+main()
